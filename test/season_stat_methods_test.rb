1e56dd3839b0f201f52a51ddde0d87173e2fbe9f
Michael Gallup <Gallup93@icloud.com>
2020-04-09 18:20:57 -0600

    added test_assemble_coaches_and_teams method

--- test/season_stat_methods_test.rb
+++ test/season_stat_methods_test.rb
@@ -1,11 +1,12 @@
 require_relative 'test_helper'
+require './lib/stat_tracker'
 require './lib/game_collection'
 require './lib/season_stat_methods'
-require 'mocha/minitest'
 
 class SeasonStatMethodsTest < Minitest::Test
   def setup
-    @games = GameCollection.new("./test/fixtures/truncated_games2.csv")
+    @games = GameCollection.new('./test/fixtures/truncated_games2.csv')
+    # @games = GameCollection.new('./data/games.csv')
     @season_stat_methods = SeasonStatMethods.new(@games)
   end
 
@@ -18,6 +19,21 @@ class SeasonStatMethodsTest < Minitest::Test
   def test_all_games_of_season
     assert_equal "2012020122", @season_stat_methods.all_games_of_season("20122013").first.game_id
     assert_equal "2012020495", @season_stat_methods.all_games_of_season("20122013").last.game_id
+  end
+
+  def test_assemble_coaches_and_teams
+    coaches_1213 = { "3"=>"John Tortorella", "6"=>"Claude Julien",
+      "5"=>"Dan Bylsma", "17"=>"Mike Babcock", "16"=>"Joel Quenneville",
+      "9"=>"Paul MacLean", "8"=>"Michel Therrien", "30"=>"Mike Yeo",
+      "26"=>"Darryl Sutter", "19"=>"Ken Hitchcock", "24"=>"Bruce Boudreau",
+      "2"=>"Jack Capuano", "15"=>"Adam Oates", "20"=>"Glen Gulutzan",
+      "14"=>"Jon Cooper", "28"=>"Peter DeBoer", "4"=>"Dave Hakstol",
+      "21"=>"Patrick Roy", "25"=>"Lindy Ruff", "13"=>"Gerard Gallant",
+      "18"=>"Peter Laviolette", "10"=>"Mike Babcock", "29"=>"John Tortorella",
+      "52"=>"Paul Maurice", "54"=>"Gerard Gallant", "1"=>"John Hynes",
+      "23"=>"John Tortorella", "12"=>"Kirk Muller", "27"=>"Dave Tippett",
+      "7"=>"Ron Rolston", "22"=>"Ralph Krueger", "53"=>"Dave Tippett" }
+    assert_equal coaches_1213, @season_stat_methods.assemble_coaches_and_teams
   end
 
   def test_find_winner_loser_of_game
@@ -35,8 +51,10 @@ class SeasonStatMethodsTest < Minitest::Test
   end
 
   def test_worst_coach
-    assert_equal "Gerard Gallant", @season_stat_methods.worst_coach("20122013")
+    assert_equal "Gerard Gallant", @season_stat_methods.worst_coach("20122013")#20132014
   end
+
+
 
 
 
